c8e9cd3bba48bcda2afa05db18768ba0d8da0d80
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-05-03 22:53:02 +0200

    [add] seperate food

--- Data/db.php
+++ Data/db.php
@@ -3,18 +3,95 @@
 require_once __DIR__ . '/../Model/Product.php';
 require_once __DIR__ . '/../Model/Category.php';
 
-$foodProduct = [
-  new Product('image01.jpg', 'Croccantini per Cani', 20.99, $categoryCani, 'food'),
-  new Product('image01.jpg', 'Croccantini per Gatti', 20.99, $categoryGatti, 'food')
-];
+$db = [
+  $dogFood = [
+    new Product(
+      'https://arcaplanet.vtexassets.com/arquivos/ids/224338-200-200/virtus-rustic-cane-adult.jpg?v=637454741671700000',
+      'Croccantini per Cani',
+      20.99,
+      $categoryCani,
+      'food'
+    ),
+
+    new Product(
+      'https://arcaplanet.vtexassets.com/arquivos/ids/282088-200-200/purina-one-cane-adult-agnello-riso.jpg?v=638132667548900000',
+      'Croccantini per Cani',
+      25.99,
+      $categoryCani,
+      'food'
+    ),
+
+    new Product(
+      'https://arcaplanet.vtexassets.com/arquivos/ids/276615-200-200/MAIN.jpg?v=638034911955130000',
+      'Alimento per Cani',
+      25.99,
+      $categoryCani,
+      'food'
+    ),
+  ],
+
+  $catFood = [
+    new Product(
+      'https://arcaplanet.vtexassets.com/arquivos/ids/269928-200-200/virtus-cat-sterilised-nutrition-selection-salmone-1-8-kg.jpg?v=637840506444500000',
+      'Croccantini per Gatti',
+      20.99,
+      $categoryGatti,
+      'food'
+    ),
+
+    new Product(
+      'https://arcaplanet.vtexassets.com/arquivos/ids/299364-200-200/natural-trainer-gatto-sterilised-salmone-10112328--1-.jpg?v=638482560030700000',
+      'Croccantini per Gatti',
+      20.99,
+      $categoryGatti,
+      'food'
+    ),
+
+    new Product(
+      'https://arcaplanet.vtexassets.com/arquivos/ids/285499-200-200/1338015-1-416x599--1-.jpg?v=638197416605200000',
+      'Alimento per Gatti',
+      20.99,
+      $categoryGatti,
+      'food'
+    )
+  ],
 
 
-$toyProduct = [
-  new Product('dog_toy.jpg', 'Giocattolo per Cani', 9.99, $categoryCani, 'toy'),
-  new Product('cat_toy.jpg', 'Giocattolo per Gatti', 9.99, $categoryGatti, 'toy')
-];
+  $dogToys = [
+    new Product(
+      'https://arcaplanet.vtexassets.com/arquivos/ids/273069-200-200/LOVEDI-GIOCO-CANE-PELUCHE-CORDA-JUNGLE-ELEFANTE.jpg?v=637920012649330000',
+      'Giocattolo per Cani',
+      9.99,
+      $categoryCani,
+      'toy'
+    ),
+  ],
 
-$bedProduct = [
-  new Product('dog_bed.jpg', 'Cuccia per Cani', 29.99, $categoryCani, 'bed'),
-  new Product('gatti_bed.jpg', 'Cuccia per Gatti', 29.99, $categoryGatti, 'bed'),
+  $catToys = [
+    new Product(
+      'https://arcaplanet.vtexassets.com/arquivos/ids/291708-200-200/lovedi-gioco-gatto-xmas-bacchetta-topo-10170309.jpg?v=638351387471670000',
+      'Giocattolo per Gatti',
+      9.99,
+      $categoryGatti,
+      'toy'
+    )
+  ],
+
+  $bedProduct = [
+    new Product(
+      'https://arcaplanet.vtexassets.com/arquivos/ids/216810-200-200/https---www.arcaplanet.it-media-catalog-product--b-a-baita-ferc14_1.jpg?v=637454582539930000',
+      'Cuccia per Cani',
+      29.99,
+      $categoryCani,
+      'bed'
+    ),
+    new Product(
+      '
+    https://arcaplanet.vtexassets.com/arquivos/ids/265702-200-200/PHO_PRO_CAT_CLIP_49920-01-49923-1_-SALL_-APRKU_-V1.jpg?v=637737941822300000',
+      'Cuccia per Gatti',
+      29.99,
+      $categoryGatti,
+      'bed'
+    ),
+  ],
 ];
--- index.php
+++ index.php
@@ -5,9 +5,9 @@ require_once __DIR__ . '/Model/Category.php';
 require_once __DIR__ . '/Data/db.php';
 
 
-var_dump($foodProduct);
-var_dump($toyProduct);
-var_dump($bedProduct)
+// var_dump($foodProduct);
+// var_dump($toyProduct);
+// var_dump($bedProduct)
 ?>
 
 <!DOCTYPE html>
@@ -17,50 +17,76 @@ var_dump($bedProduct)
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
   <title>Modello</title>
 </head>
 
 <body>
   <div class="container my-5">
-    <h1> php-oop-2</h1>
+    <h1 class="text-center my-5 fw-bolder text-white">Pet's Shop</h1>
 
-    <div class="d-flex flex-wrap justify-content-center">
+    <div class="row d-flex flex-wrap justify-content-center">
 
-      <?php foreach ($foodProduct as $product) : ?>
-        <div class="card m-3">
-          <img src="<?php echo $product->image ?>" alt="Product Image">
-          <div class="card-body">
-            <h2><?php echo $product->name ?></h2>
-            <p>Prezzo: <?php echo $product->price ?> €</p>
-            <p>Categoria: <?php echo $product->category->name ?></p>
-            <p>Tipo: <?php echo $product->type ?></p>
+      <h3 class="text-white fw-bold text-center">Food</h3>
+      <h4 class="fw-bold text-white">Dog's</h4>
+      <div class="col-12 d-flex">
+        <?php foreach ($dogFood as $product) : ?>
+          <div class="card m-3">
+            <img class="img-fluid" src="<?php echo $product->image ?>" alt="Product Image">
+            <div class="card-body">
+              <h2><?php echo $product->name ?></h2>
+              <p>Prezzo: <?php echo $product->price ?> €</p>
+              <p>Categoria: <?php echo $product->category->name ?></p>
+              <p>Tipo: <?php echo $product->type ?></p>
+            </div>
           </div>
-        </div>
-      <?php endforeach; ?>
+        <?php endforeach; ?>
+      </div>
 
-      <?php foreach ($toyProduct as $product) : ?>
-        <div class="card m-3">
-          <img src="<?php echo $product->image ?>" alt="Product Image">
-          <div class="card-body">
-            <h2><?php echo $product->name ?></h2>
-            <p>Prezzo: <?php echo $product->price ?> €</p>
-            <p>Categoria: <?php echo $product->category->name ?></p>
-            <p>Tipo: <?php echo $product->type ?></p>
+      <h4 class="fw-bold text-white">Cat's</h4>
+      <div class="col-12 d-flex">
+        <?php foreach ($catFood as $product) : ?>
+          <div class="card m-3">
+            <img class="img-fluid" src="<?php echo $product->image ?>" alt="Product Image">
+            <div class="card-body">
+              <h2><?php echo $product->name ?></h2>
+              <p>Prezzo: <?php echo $product->price ?> €</p>
+              <p>Categoria: <?php echo $product->category->name ?></p>
+              <p>Tipo: <?php echo $product->type ?></p>
+            </div>
           </div>
-        </div>
-      <?php endforeach; ?>
+        <?php endforeach; ?>
+      </div>
 
-      <?php foreach ($bedProduct as $product) : ?>
-        <div class="card m-3">
-          <img src="<?php echo $product->image ?>" alt="Product Image">
-          <div class="card-body">
-            <h2><?php echo $product->name ?></h2>
-            <p>Prezzo: <?php echo $product->price ?> €</p>
-            <p>Categoria: <?php echo $product->category->name ?></p>
-            <p>Tipo: <?php echo $product->type ?></p>
+      <h3 class="text-white fw-bold">Toys</h3>
+      <div class="col-12 d-flex">
+        <?php foreach ($toyProduct as $product) : ?>
+          <div class="card m-3">
+            <img src="<?php echo $product->image ?>" alt="Product Image">
+            <div class="card-body">
+              <h2><?php echo $product->name ?></h2>
+              <p>Prezzo: <?php echo $product->price ?> €</p>
+              <p>Categoria: <?php echo $product->category->name ?></p>
+              <p>Tipo: <?php echo $product->type ?></p>
+            </div>
           </div>
-        </div>
-      <?php endforeach; ?>
+        <?php endforeach; ?>
+      </div>
+
+      <h3 class="text-white fw-bold">Kennel</h3>
+      <div class="col-12 d-flex">
+        <?php foreach ($bedProduct as $product) : ?>
+          <div class="card m-3">
+            <img src="<?php echo $product->image ?>" alt="Product Image">
+            <div class="card-body">
+              <h2><?php echo $product->name ?></h2>
+              <p>Prezzo: <?php echo $product->price ?> €</p>
+              <p>Categoria: <?php echo $product->category->name ?></p>
+              <p>Tipo: <?php echo $product->type ?></p>
+            </div>
+          </div>
+        <?php endforeach; ?>
+      </div>
 
     </div>
   </div>
