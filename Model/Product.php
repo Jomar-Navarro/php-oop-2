a9623862e49f6f07766b239d7c9a8253fce03477
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-05-03 14:47:38 +0200

    [add] product

---
+++ Model/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+class Product
+{
+  public $image;
+  public $name;
+  public $price;
+  public $category;
+  public $type;
+
+  public function __construct(string $_image, string $_name, int $_price, string $_category, string $_type)
+  {
+    $this->image = $_image;
+    $this->name = $_name;
+    $this->price = $_price;
+    $this->category = $_category;
+    $this->type = $_type;
+  }
+}
